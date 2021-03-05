1720a3baaa39dde4aa20430743b9a8e5b3bf70c0
Mohan Patil <mpatil@karrostech.com>
2021-03-05 17:08:22 -0600

    Base version

---
+++ debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
@@ -0,0 +1,13 @@
+package com.karrostech.services.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumPlaybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumPlaybookApplication.class, args);
+	}
+
+}
