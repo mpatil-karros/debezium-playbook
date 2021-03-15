0b56f87e15af4a581c63c8ce6bee275273e53730
Mohan Patil <mpatil@karrostech.com>
2021-03-14 21:25:09 -0500

    Working Module

--- debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
+++ debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
@@ -1,13 +1,26 @@
 package com.karrostech.services.infra;
 
+import com.karrostech.services.infra.properties.DbzProperties;
+import com.karrostech.services.infra.properties.TopicProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class DebeziumPlaybookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DebeziumPlaybookApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(DebeziumPlaybookApplication.class, args);
+
+		DbzProperties debeziumProperties = applicationContext.getBean(DbzProperties.class);
+		log.info("Debezium Configuration: {}", debeziumProperties.toString());
+
+		TopicProperties topicProperties = applicationContext.getBean(TopicProperties.class);
+		log.info("TopicProperties Configuration: {}", topicProperties.toString());
+
+
 	}
 
 }
