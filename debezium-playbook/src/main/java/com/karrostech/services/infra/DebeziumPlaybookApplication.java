fb33627988475230a717ab0d31ec1c6255efd035
Mohan Patil <mpatil@karrostech.com>
2021-03-16 15:33:49 -0500

    Null Properties coming in

--- debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
+++ debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
@@ -1,7 +1,5 @@
 package com.karrostech.services.infra;
 
-import com.karrostech.services.infra.properties.DbzProperties;
-import com.karrostech.services.infra.properties.TopicProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +12,6 @@ public class DebeziumPlaybookApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(DebeziumPlaybookApplication.class, args);
 
-		DbzProperties debeziumProperties = applicationContext.getBean(DbzProperties.class);
-		log.info("Debezium Configuration: {}", debeziumProperties.toString());
-
-		TopicProperties topicProperties = applicationContext.getBean(TopicProperties.class);
-		log.info("TopicProperties Configuration: {}", topicProperties.toString());
 
 
 	}
