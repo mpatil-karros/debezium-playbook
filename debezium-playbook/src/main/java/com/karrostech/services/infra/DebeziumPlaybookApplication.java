3ef15712c63e970b2593ff4e494f50a794df3432
Mohan Patil <mpatil@karrostech.com>
2021-03-16 23:23:06 -0500

    Working Module

--- debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
+++ debezium-playbook/src/main/java/com/karrostech/services/infra/DebeziumPlaybookApplication.java
@@ -1,19 +1,13 @@
 package com.karrostech.services.infra;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@Slf4j
 public class DebeziumPlaybookApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(DebeziumPlaybookApplication.class, args);
-
-
-
+		SpringApplication.run(DebeziumPlaybookApplication.class, args);
 	}
 
 }
